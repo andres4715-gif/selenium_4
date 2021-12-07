419de921cbf0984b9df5515f9ac2e2180c171509
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2021-12-07 09:57:18 -0500

    first test scenario

---
+++ src/test/java/test/FirstTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class FirstTest {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.close();
+    }
+}
