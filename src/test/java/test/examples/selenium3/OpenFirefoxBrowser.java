693d1044d0e1864280006fe779f8dfeee22930ed
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-02-20 16:12:58 -0500

    new changes

--- src/test/java/test/examples/selenium3/OpenFirefoxBrowser.java
+++ src/test/java/test/examples/selenium3/OpenFirefoxBrowser.java
@@ -1,8 +1,6 @@
 package test.examples.selenium3;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class OpenFirefoxBrowser {
