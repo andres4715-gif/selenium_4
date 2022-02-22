30feb59bd9356ff6b65d2b3508db803175f20cd2
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-02-22 16:28:48 -0500

    new pom feature

--- src/test/java/PomPractice/pages/Setup.java
+++ src/test/java/PomPractice/pages/Setup.java
@@ -79,4 +79,11 @@ public class Setup {
         terms.click();
         send.click();
     }
+
+    public static void table(WebDriver driver) {
+        String baseUrl = "http://demo.guru99.com/test/write-xpath-table.html";
+        driver.get(baseUrl);
+        String innerText = driver.findElement(By.xpath("//table/tbody/tr[2]/td[2]")).getText();
+        System.out.println("this cell is: " + innerText);
+    }
 }
