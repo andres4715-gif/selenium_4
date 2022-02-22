5764b98d65a9390adc9bc4ba14362b503af3adf6
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-02-22 13:07:43 -0500

    new pom feature

--- src/test/java/PomPractice/pages/Setup.java
+++ src/test/java/PomPractice/pages/Setup.java
@@ -3,6 +3,9 @@ package PomPractice.pages;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Setup {
 
@@ -13,6 +16,21 @@ public class Setup {
     public static void gettingPageAndClick(WebDriver driver) throws InterruptedException {
         driver.get("http://demo.guru99.com/test/ajax.html");
         driver.findElement(By.id("no")).click();
+        Thread.sleep(3000);
+        driver.close();
+    }
+
+    public static void gettingElementsValues(WebDriver driver) throws InterruptedException {
+        driver.get("http://demo.guru99.com/test/ajax.html");
+        List<WebElement> elements = driver.findElements(By.name("name"));
+        System.out.println("Number of elements: " + elements.size());
+
+        for(WebElement element : elements) {
+            System.out.println(element.getAttribute("value"));
+        }
+    }
+
+    public static void after(WebDriver driver) throws InterruptedException {
         Thread.sleep(3000);
         driver.close();
     }
---
+++ src/test/java/test/examples/selenium3/Guru99PageObjectModelFindElements.java
@@ -0,0 +1,27 @@
+package test.examples.selenium3;
+
+import PomPractice.pages.Setup;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class Guru99PageObjectModelFindElements {
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        Setup.mySetup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void after() throws InterruptedException {
+        Setup.after(driver);
+    }
+
+    @Test
+    public static void openGuruPage() throws InterruptedException {
+        Setup.gettingElementsValues(driver);
+    }
+}
