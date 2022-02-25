b78099babf1efccb37b50267543ee468c06dddb8
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-02-25 18:26:44 -0500

    new array example

---
+++ src/main/java/javaPractice/arrays/example2.java
@@ -0,0 +1,39 @@
+// Ejemplo: Pedir 10 números por teclado y obtener la suma,
+// el número menor, el número  mayor
+
+package javaPractice.arrays;
+
+import java.util.Scanner;
+
+public class example2 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int mayor = 0;
+        int menor = 0;
+
+        int[] miArreglo = new int[3];
+        for (int i = 0; i < miArreglo.length; i++) {
+            System.out.println("Ingrese el dato de la posición " + (i + 1) + ": ");
+            miArreglo[i] = entrada.nextInt();
+        }
+
+        // realizar la suma, promedio y el mayor numero
+        for (int i = 0; i < miArreglo.length; i++) {
+            if (mayor < miArreglo[i]) {
+                mayor = miArreglo[i];
+            }
+        }
+        System.out.println("El numero mayor es: " + mayor);
+
+        menor = mayor;
+        // buscar el menor
+        for (int i = 0; i < miArreglo.length; i++) {
+            if (menor > miArreglo[i]) {
+                menor = miArreglo[i];
+            }
+        }
+        System.out.println("El numero menor es: " + menor);
+    }
+}
