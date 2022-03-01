029bba8877f7d39be12d8123fa8caed989e097ca
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-01 10:53:28 -0500

    new practice ready

--- src/main/java/javaPractice/POO/clasesAbtractas/example3/Main.java
+++ src/main/java/javaPractice/POO/clasesAbtractas/example3/Main.java
@@ -1,11 +1,20 @@
 package javaPractice.POO.clasesAbtractas.example3;
 
 public class Main {
+    /*
+    Este ejercicio tiene polimorfismo aplicado.
+    Se implementa el Polimorfismo ya que se crea el objeto a1 pero luego se llaman los constructores de cada clase
+
+    Vehiculo a1 = new Automovil(<datos del constructor>);
+    a1 = new Motocicleta(<datos del constructor>);
+    a1 = new Camion(<datos del constructor>);
+
+    */
 
     public static void main(String[] args) {
-        Automovil a1 = new Automovil("Automovil", 4, false, "Mazda", "Mazda3", 2013, "Gris", 2000, true, false);
+        Vehiculo a1 = new Automovil("Automovil", 4, false, "Mazda", "Mazda3", 2013, "Gris", 2000, true, false);
         System.out.println("----Automovil-----");
-        a1.puertas(4);
+        ((Automovil) a1).puertas(4);
         System.out.println("El " + a1.tipoVehiculo + " tiene " + a1.cantidadLlantas(4) + " llantas");
         System.out.println("El " + a1.tipoVehiculo + " frena seguro: " + a1.frenarSeguro());
         System.out.println("El " + a1.tipoVehiculo + " tiene pito: " + a1.pitar());
@@ -13,20 +22,20 @@ public class Main {
 
         System.out.println();
         System.out.println("----Motocicleta-----");
-        Motocicleta m1 = new Motocicleta("Motocicleta", 2, false, "Auteco", "Agility", 2022, "Negro", 150, true, true);
-        m1.agarraderas(1);
-        System.out.println("La " + m1.tipoVehiculo + " tiene " + m1.cantidadLlantas(2) + " llantas");
-        System.out.println("La " + m1.tipoVehiculo + " frena seguro: " + m1.frenarSeguro());
-        System.out.println("La " + m1.tipoVehiculo + " tiene pito: " + m1.pitar());
-        System.out.println("La " + m1.tipoVehiculo + " puede arrancar: " + m1.moverse());
+        a1 = new Motocicleta("Motocicleta", 2, false, "Auteco", "Agility", 2022, "Negro", 150, true, true);
+        ((Motocicleta) a1).agarraderas(1);
+        System.out.println("La " + a1.tipoVehiculo + " tiene " + a1.cantidadLlantas(2) + " llantas");
+        System.out.println("La " + a1.tipoVehiculo + " frena seguro: " + a1.frenarSeguro());
+        System.out.println("La " + a1.tipoVehiculo + " tiene pito: " + a1.pitar());
+        System.out.println("La " + a1.tipoVehiculo + " puede arrancar: " + a1.moverse());
 
         System.out.println();
         System.out.println("----Camion-----");
-        Camion c1 = new Camion("Camion", 18, false, "Kenword", "AS678", 1929, "Negro", 35000, true, false);
-        c1.cantidadTrompetas(7);
-        System.out.println("El " + c1.tipoVehiculo + " tiene " + c1.cantidadLlantas(18) + " llantas");
-        System.out.println("El " + c1.tipoVehiculo + " frena seguro: " + c1.frenarSeguro());
-        System.out.println("El " + c1.tipoVehiculo + " tiene pito: " + c1.pitar());
-        System.out.println("EL " + c1.tipoVehiculo + " puede arrancar: " + c1.moverse());
+        a1 = new Camion("Camion", 18, false, "Kenword", "AS678", 1929, "Negro", 35000, true, false);
+        ((Camion) a1).cantidadTrompetas(7);
+        System.out.println("El " + a1.tipoVehiculo + " tiene " + a1.cantidadLlantas(18) + " llantas");
+        System.out.println("El " + a1.tipoVehiculo + " frena seguro: " + a1.frenarSeguro());
+        System.out.println("El " + a1.tipoVehiculo + " tiene pito: " + a1.pitar());
+        System.out.println("EL " + a1.tipoVehiculo + " puede arrancar: " + a1.moverse());
     }
 }
