005ef2217b6d685dec26c486b239c79bf58c0d34
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-03 08:29:16 -0500

    new abstrac done

--- src/main/java/javaPractice/POO/clasesAbtractas/example4/Main.java
+++ src/main/java/javaPractice/POO/clasesAbtractas/example4/Main.java
@@ -4,43 +4,43 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("***** WashingMachine =====");
-        WashingMachine obj1 = new WashingMachine(1, true, "LG", 8997);
-        System.out.println("The wachingMmchine is conected: " + obj1.conected());
+        HomeAppliances obj1 = new WashingMachine(1, true, "LG", 8997);
+        System.out.println("The wachingMmchine is connected: " + obj1.conected());
         System.out.println("The wachingMmchine is working: " + obj1.working());
         System.out.println("The wachingMmchine does: " + obj1.action());
-        System.out.println("The wachingMmchine serial is: " + obj1.serial("UIJHJHYU89876"));
-        System.out.println("The wachingMmchine code is: " + obj1.code("C0001"));
-        System.out.println("The tradeMark is: " + obj1.tradeMark);
-        System.out.println("The model is: " + obj1.model);
+        System.out.println("The wachingMmchine serial is: " + ((WashingMachine) obj1).serial("UIJHJHYU89876"));
+        System.out.println("The wachingMmchine code is: " + ((WashingMachine) obj1).code("C0001"));
+        System.out.println("The tradeMark is: " + ((WashingMachine) obj1).tradeMark);
+        System.out.println("The model is: " + ((WashingMachine) obj1).model);
 
         System.out.println("***** Microware =====");
-        Microware obj2 = new Microware(1, false, "SAMSUNG", 8976);
-        System.out.println("The Microware is conected: " + obj2.conected());
-        System.out.println("The Microware is working: " + obj2.working());
-        System.out.println("The Microware does: " + obj2.action());
-        System.out.println("The Microware serial is: " + obj2.serial("S8987UY7"));
-        System.out.println("The Microware code is: " + obj2.code("COOO99"));
-        System.out.println("The tradeMark is: " + obj2.tradeMark);
-        System.out.println("The model is: " + obj2.model);
+        obj1 = new Microware(1, false, "SAMSUNG", 8976);
+        System.out.println("The Microware is connected: " + obj1.conected());
+        System.out.println("The Microware is working: " + obj1.working());
+        System.out.println("The Microware does: " + obj1.action());
+        System.out.println("The Microware serial is: " + ((Microware) obj1).serial("S8987UY7"));
+        System.out.println("The Microware code is: " + ((Microware) obj1).code("COOO99"));
+        System.out.println("The tradeMark is: " + ((Microware) obj1).tradeMark);
+        System.out.println("The model is: " + ((Microware) obj1).model);
 
         System.out.println("***** Stove =====");
-        Stove obj3 = new Stove(1, true, "HACEB", 11111);
-        System.out.println("The Stove is conected: " + obj3.conected());
-        System.out.println("The Stove is working: " + obj3.working());
-        System.out.println("The Stove does: " + obj3.action());
-        System.out.println("The Stove serial is: " + obj3.serial("TIOIKJK8"));
-        System.out.println("The Stove code is: " + obj3.code("STOVE909IU8"));
-        System.out.println("The tradeMark is: " + obj3.tradeMark);
-        System.out.println("The model is: " + obj3.model);
+        obj1 = new Stove(1, true, "HACEB", 11111);
+        System.out.println("The Stove is connected: " + obj1.conected());
+        System.out.println("The Stove is working: " + obj1.working());
+        System.out.println("The Stove does: " + obj1.action());
+        System.out.println("The Stove serial is: " + ((Stove) obj1).serial("TIOIKJK8"));
+        System.out.println("The Stove code is: " + ((Stove) obj1).code("STOVE909IU8"));
+        System.out.println("The tradeMark is: " + ((Stove) obj1).tradeMark);
+        System.out.println("The model is: " + ((Stove) obj1).model);
 
         System.out.println("***** ElectricSweeper =====");
-        ElectricSweeper obj4 = new ElectricSweeper(0, false, "XIAOMI", 2222);
-        System.out.println("The ElectricSweeper is conected: " + obj4.conected());
-        System.out.println("The ElectricSweeper is working: " + obj4.working());
-        System.out.println("The ElectricSweeper does: " + obj4.action());
-        System.out.println("The ElectricSweeper serial is: " + obj4.serial("ST"));
-        System.out.println("The ElectricSweeper code is: " + obj4.code("Sweeper909IKK"));
-        System.out.println("The tradeMark is: " + obj4.tradeMark);
-        System.out.println("The model is: " + obj4.model);
+        obj1 = new ElectricSweeper(0, false, "XIAOMI", 2222);
+        System.out.println("The ElectricSweeper is connected: " + obj1.conected());
+        System.out.println("The ElectricSweeper is working: " + obj1.working());
+        System.out.println("The ElectricSweeper does: " + obj1.action());
+        System.out.println("The ElectricSweeper serial is: " + ((ElectricSweeper) obj1).serial("ST"));
+        System.out.println("The ElectricSweeper code is: " + ((ElectricSweeper) obj1).code("Sweeper909IKK"));
+        System.out.println("The tradeMark is: " + ((ElectricSweeper) obj1).tradeMark);
+        System.out.println("The model is: " + ((ElectricSweeper) obj1).model);
     }
 }
