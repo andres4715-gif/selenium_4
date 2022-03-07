84a7372065f04e3ae0f6600f5f3a4a6b6ddbee59
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-07 08:28:16 -0500

    new data ready

---
+++ src/main/java/javaPractice/ejercicios/stream/completeExample1/StreamsApp.java
@@ -0,0 +1,50 @@
+package javaPractice.ejercicios.stream.completeExample1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsApp {
+
+    String[] listaData = {"Mito", "Code", "Jaime", "MitoCode"};
+    String[] numerosData = {"1", "2", "3", "4"};
+
+    List<String> lista = Arrays.asList(listaData);
+    List<String> numeros = Arrays.asList(numerosData);
+
+
+    void filtrar() {
+        lista.stream()
+                .filter(x -> x.startsWith("M"))
+                .forEach(System.out::println);
+    }
+
+    void ordenar() {
+        lista.stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    void transformar() {
+        lista.stream()
+                .map(x -> x.toUpperCase())
+                .forEach(System.out::println);
+    }
+
+    void transformaNumeros() {
+        numeros.stream()
+                .map(x -> Integer.parseInt(x) + 1)
+                .forEach(System.out::println);
+    }
+
+    void limitar() {
+        lista.stream()
+                .limit(2)
+                .forEach(System.out::println);
+    }
+
+    void contar() {
+        long x = lista.stream()
+                .count();
+        System.out.println(x);
+    }
+}
---
+++ src/main/java/javaPractice/ejercicios/stream/completeExample1/main.java
@@ -0,0 +1,13 @@
+package javaPractice.ejercicios.stream.completeExample1;
+
+public class main {
+    public static void main(String[] args) {
+        StreamsApp app = new StreamsApp();
+        // app.filtrar();
+        // app.ordenar();
+        // app.transformar();
+        // app.transformaNumeros();
+        // app.limitar();
+        app.contar();
+    }
+}
