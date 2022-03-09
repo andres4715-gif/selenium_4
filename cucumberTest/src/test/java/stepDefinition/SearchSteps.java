03c1b10de78eda198a70d1af7ed5d86170d64e0c
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-09 17:43:21 -0500

    first upload

--- cucumberTest/src/test/java/stepDefinition/SearchSteps.java
+++ cucumberTest/src/test/java/stepDefinition/SearchSteps.java
@@ -15,24 +15,24 @@ public class SearchSteps {
 
     WebDriver driver;
 
-    @Given("the user is in the index page")
+    @Given("^the user is in the index page$")
     public void theUserIsInTheIndexPage() {
         System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
         driver = new FirefoxDriver();
         driver.get("http://automationpractice.com/index.php");
     }
 
-    @When("the user enters (.*) in the search bar")
+    @When("^the user enters (.*) in the search bar$")
     public void the_user_enters_dresses_in_the_search_bar(String clothes) {
         driver.findElement(By.id("search_query_top")).sendKeys(clothes);
     }
 
-    @When("the user clicks the search button")
+    @When("^the user clicks the search button$")
     public void theUserClicksTheSearchButton() {
         driver.findElement(By.name("submit_search")).click();
     }
 
-    @Then("the (.*) page appears")
+    @Then("^the (.*) page appears$")
     public void the_dresses_page_appears(String clothes) throws InterruptedException {
         String titleResultPage = driver.findElement(By.cssSelector("[class='lighter']")).getText();
         Assert.assertEquals(titleResultPage, "\"" + clothes.toUpperCase() + "\"");
