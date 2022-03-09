f3e52b3316f60ecef314449c68020b92ca615ea3
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-09 11:03:14 -0500

    new feature

---
+++ src/main/java/javaPractice/stream/completeExample2/Main.java
@@ -0,0 +1,14 @@
+package javaPractice.stream.completeExample2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> person = new ArrayList<String>();
+        person.add("Andres");
+        System.out.println(person.get(0));
+    }
+}
