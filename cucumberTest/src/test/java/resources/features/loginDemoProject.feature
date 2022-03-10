bb10291f78f2aedf12a5494ab047078e909d199f
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-10 12:17:36 -0500

    first upload

--- cucumberTest/src/test/java/resources/features/loginDemoProject.feature
+++ cucumberTest/src/test/java/resources/features/loginDemoProject.feature
@@ -2,14 +2,19 @@
 #Date: 10/03/2022
 #Description: Practicing Cucumber feature and execution.
 
-  @SmokeRegression
+@SmokeRegression
 Feature: Test Login functionality
 
   @smoketestProject
-  Scenario: Check login is successful on the page
+  Scenario Outline: Check login is successful on the page
     Given the user is on the login project page
-    When the user enters the valid username and password
+    When the user enters the valid <username> and <password>
     And taps over login button
     Then the user can check if this is valid credentials
+    Examples:
+      | username | password |
+      | Raghav   | 12345    |
+      | Andres   | 12345    |
+      | Carlos   | 12345    |
 
 
--- cucumberTest/src/test/java/stepDefinition/loginDemoProject.java
+++ cucumberTest/src/test/java/stepDefinition/loginDemoProject.java
@@ -1,5 +1,7 @@
 package stepDefinition;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -15,20 +17,32 @@ import java.time.Duration;
 public class loginDemoProject {
     WebDriver driver;
 
-    @Given("^the user is on the login project page$")
-    public void the_user_is_on_the_login_project_page() {
+    @Before
+    public void driverOptions() {
         System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
         driver = new FirefoxDriver();
-        driver.get("https://example.testproject.io/web/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        driver.get("https://example.testproject.io/web/");
     }
 
-    @When("^the user enters the valid username and password$")
-    public void the_user_enters_the_valid_username_and_password() {
-        driver.findElement(By.id("name")).sendKeys("Raghav");
-        driver.findElement(By.id("password")).sendKeys("12345");
+    @After
+    public void afterTesting() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+
+    @Given("^the user is on the login project page$")
+    public void the_user_is_on_the_login_project_page() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+    }
+
+    @When("^the user enters the valid (.*) and (.*)$")
+    public void the_user_enters_the_valid_username_and_password(String username, String password) {
+        driver.findElement(By.id("name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.manage().window().maximize();
 
     }
 
@@ -38,12 +52,8 @@ public class loginDemoProject {
     }
 
     @Then("^the user can check if this is valid credentials$")
-    public void the_user_can_check_if_this_is_valid_credentials() throws InterruptedException {
+    public void the_user_can_check_if_this_is_valid_credentials() {
         boolean checkLogOut = driver.findElement(By.id("logout")).isDisplayed();
         Assert.assertTrue(checkLogOut);
-
-        Thread.sleep(1000);
-        driver.quit();
-
     }
 }
