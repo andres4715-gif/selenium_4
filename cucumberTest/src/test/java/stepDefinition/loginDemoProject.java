f5dfa7deb9c7e4a3865d4d83b4c5d7defb5b68c4
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-10 12:38:13 -0500

    first upload

--- cucumberTest/src/test/java/stepDefinition/loginDemoProject.java
+++ cucumberTest/src/test/java/stepDefinition/loginDemoProject.java
@@ -13,7 +13,6 @@ import org.testng.Assert;
 
 import java.time.Duration;
 
-
 public class loginDemoProject {
     WebDriver driver;
 
@@ -22,28 +21,26 @@ public class loginDemoProject {
         System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://example.testproject.io/web/");
-    }
-
-    @After
-    public void afterTesting() throws InterruptedException {
-        Thread.sleep(1000);
-        driver.quit();
-    }
-
-
-    @Given("^the user is on the login project page$")
-    public void the_user_is_on_the_login_project_page() {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
     }
 
+    @After
+    public void afterTest() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+    @Given("^the user is on the login project page$")
+    public void the_user_is_on_the_login_project_page() {
+        driver.get("https://example.testproject.io/web/");
+    }
+
     @When("^the user enters the valid (.*) and (.*)$")
-    public void the_user_enters_the_valid_username_and_password(String username, String password) {
+    public void the_user_enters_the_valid_username_and_password(String username, String password) throws InterruptedException {
         driver.findElement(By.id("name")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
-        driver.manage().window().maximize();
-
+        Thread.sleep(1000);
     }
 
     @And("^taps over login button$")
@@ -52,7 +49,7 @@ public class loginDemoProject {
     }
 
     @Then("^the user can check if this is valid credentials$")
-    public void the_user_can_check_if_this_is_valid_credentials() {
+    public void the_user_can_check_if_this_is_valid_credentials() throws InterruptedException {
         boolean checkLogOut = driver.findElement(By.id("logout")).isDisplayed();
         Assert.assertTrue(checkLogOut);
     }
