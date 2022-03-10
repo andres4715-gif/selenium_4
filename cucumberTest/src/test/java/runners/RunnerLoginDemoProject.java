2243df98bd85b5c5f2ffcf80cf74e2c630c8608d
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-03-10 14:34:06 -0500

    first upload

--- cucumberTest/src/test/java/runners/RunnerLoginDemoProject.java
+++ cucumberTest/src/test/java/runners/RunnerLoginDemoProject.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/java/resources/features/loginDemoProject.feature"
         , glue = "stepDefinition"
         , monochrome = true
-        , plugin = {"pretty","html:target/HTMLReports/report.html"}
+        , plugin = {"pretty", "html:target/HTMLReports/report.html"}
         , tags = "" // Example => @smoketest
 )
 public class RunnerLoginDemoProject extends AbstractTestNGCucumberTests {
