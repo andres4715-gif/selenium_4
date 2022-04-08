d33dc0f7b877843092cdb36d8ec9f215297041c6
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-04-08 15:38:17 -0500

    new practice streams

--- src/main/java/javaPractice/stream/filterDemo/trabajandoConObjetos/Main.java
+++ src/main/java/javaPractice/stream/filterDemo/trabajandoConObjetos/Main.java
@@ -1,19 +1,9 @@
 package javaPractice.stream.filterDemo.trabajandoConObjetos;
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        int checkResultAmount;
-        List<Usuarios> namesList = StepDefinition.checkig();
-        checkResultAmount = namesList.size();
-        System.out.println("La lista tiene: " + checkResultAmount + " elementos");
-
-
-        for (Usuarios holasss : namesList) {
-            System.out.println(holasss.getNombre());
-        }
+        StepDefinition.checkig();
     }
 }
--- src/main/java/javaPractice/stream/filterDemo/trabajandoConObjetos/StepDefinition.java
+++ src/main/java/javaPractice/stream/filterDemo/trabajandoConObjetos/StepDefinition.java
@@ -2,13 +2,10 @@ package javaPractice.stream.filterDemo.trabajandoConObjetos;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StepDefinition {
 
-    public static List<Usuarios> checkig() {
-
-        List<Usuarios> youngPeople;
+    public static void checkig() {
 
         List<Usuarios> user = new ArrayList<>();
         user.add(new Usuarios("Andres", "Rios", 45, "M"));
@@ -16,11 +13,8 @@ public class StepDefinition {
         user.add(new Usuarios("Viviana", "Hernandez", 3, "F"));
         user.add(new Usuarios("Carmen", "Urrea", 12, "F"));
 
-        youngPeople = user
-                .stream()
+        user.stream()
                 .filter(x -> x.getEdad() <= 30)
-                .collect(Collectors.toList());
-
-        return youngPeople;
+                .forEach(x -> System.out.println(x.getNombre()));
     }
 }
