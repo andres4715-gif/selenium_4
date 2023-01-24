d06f1548b75599b3c52d54f1af71a918d88d2240
arios <andres.rios@ctg.com>
2023-01-23 22:09:09 -0500

    new practice added

--- src/test/java/test/examples/selenium3/CodilityPractice.java
+++ src/test/java/test/examples/selenium3/CodilityPractice.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CodilityPractice {
 
