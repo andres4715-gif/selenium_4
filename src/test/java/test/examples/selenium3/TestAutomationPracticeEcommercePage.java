3669f14b1bcbb6e9ed58e1ce9186920f61330309
arios <andres.rios@ctg.com>
2023-01-25 18:56:50 -0500

    adding a test case to practice selenium with ecommerce page

--- src/test/java/test/examples/selenium3/TestAutomationPracticeEcommercePage.java
+++ src/test/java/test/examples/selenium3/TestAutomationPracticeEcommercePage.java
@@ -4,14 +4,12 @@ import PomPractice.pages.Setup;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.*;
 
-public class TestAutomationPractice {
+public class TestAutomationPracticeEcommercePage {
 
     private static WebDriver driver;
 
